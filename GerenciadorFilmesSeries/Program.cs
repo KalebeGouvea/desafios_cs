86db19150a30a9685ee17ceef320f785d84def4b
Kalebe <k.gouvea@live.com>
2022-11-23 12:22:56 -0300

    Metodos testados no Main

--- GerenciadorFilmesSeries/Program.cs
+++ GerenciadorFilmesSeries/Program.cs
@@ -26,10 +26,10 @@ namespace GerenciadorFilmesSeries
             Console.WriteLine(catalogo);
             Console.WriteLine("Filmes e Series com Seu Jorge:");
             Console.WriteLine(catalogo.filtra("Jorge"));
-            //Console.WriteLine("Dramas:");
-            //Console.WriteLine(catalogo.filtra("Drama"));
-            //Console.WriteLine("Filmes e Series com Tais Araujo e Selton Mello juntos:");
-            //Console.WriteLine(catalogo.filtra("Tais").filtra("Mello"));
+            Console.WriteLine("Dramas:");
+            Console.WriteLine(catalogo.filtra("Drama"));
+            Console.WriteLine("Filmes e Series com Tais Araujo e Selton Mello juntos:");
+            Console.WriteLine(catalogo.filtra("Tais").filtra("Mello"));
             //Console.WriteLine("Filmes e Series com Tais Araujo unidos com os do Selton Mello:");
             //Console.WriteLine(catalogo.filtra("Tais").uniao(catalogo.filtra("Mello")));
         }

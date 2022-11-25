0f0aa255a0a74239a813d572198d8b167aacd696
Kalebe <k.gouvea@live.com>
2022-11-25 16:57:49 -0300

    Implementada interface ISolicitacao

---
+++ ControleProcedimento/Models/ISolicitacao.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControleProcedimento.Models
+{
+    public interface ISolicitacao
+    {
+        public double Custo();
+    }
+}

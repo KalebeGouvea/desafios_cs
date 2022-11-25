f5528c26793b93a14f157a2dabb9bbd615dbb695
Kalebe <k.gouvea@live.com>
2022-11-25 18:33:46 -0300

    Implementada classe abstrata Procedimento

---
+++ ControleProcedimento/Models/Procedimento.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControleProcedimento.Models
+{
+    public abstract class Procedimento : ISolicitacao
+    {
+        public string Nome { get; set; }
+        public int TempoMedioMinutos { get; set; }
+
+        public Procedimento(string nome, int tempoMedioMinutos)
+        {
+            Nome = nome;
+            TempoMedioMinutos = tempoMedioMinutos;
+        }
+
+        public abstract double Custo();
+    }
+}

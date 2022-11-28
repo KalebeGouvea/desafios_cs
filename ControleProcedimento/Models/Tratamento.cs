fac6b9c286942701bb575f5a72e60fa0163b505c
Kalebe <k.gouvea@live.com>
2022-11-28 11:29:07 -0300

    Corrigido tipo dos metodos e atributo Solicitacoes

---
+++ ControleProcedimento/Models/Tratamento.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControleProcedimento.Models
+{
+    public class Tratamento
+    {
+        List<Object> Solicitacoes = new List<Object>();
+
+        public void adicionaSolicitacao(ISolicitacao solicitacao)
+        {
+
+        }
+
+        public void adicionaSolicitacao(List<Object> listaSolicitacoes)
+        {
+
+        }
+    }
+}
